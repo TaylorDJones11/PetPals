92af335f533a11c30dc4558611588d6881ce68b0
Taylor J <72883059+TaylorDJones11@users.noreply.github.com>
2024-07-18 16:43:15 -0500

    setting up routes

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 const conn = require('./db/conn');
+const petRoutes = require('./routes/pets');
 
 conn();
+
+// VIEW ENGINE
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+// MIDDLEWARE
+app.use('/pets', petRoutes);
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Home route');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
---
+++ routes/pets.js
@@ -0,0 +1,19 @@
+// Adoption Pet CRUD Routes
+const express = require('express');
+const router = express.Router();
+
+// Index - GET
+
+// show - GET
+
+// new - GET
+
+// create - POST
+
+// edit - GET - form
+
+// update - PUT
+
+// destroy - DELETE
+
+module.exports = router;
